29512163fd7ec55fb17ae3fda4f46a7c6db90af2
Frank Martinez <frankm-7@tamu.edu>
2023-01-25 23:53:58 -0600

    make it better

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <SideNav />
       <div className='Content'>
-        <MainContent />
+        {/* <MainContent /> */}
       </div>
     </div>
   );
