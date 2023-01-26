bde7963d84ab746d83cf76c81cd770cbd91e4577
Frank Martinez <frankm-7@tamu.edu>
2023-01-26 11:48:12 -0600

    testing database

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-# import flask
 from flask import Flask, render_template,send_from_directory,request, jsonify, make_response
 from flask_cors import CORS, cross_origin
+from firebase_admin import credentials, firestore, initialize_app
+
 
 # create the application object
 app = Flask(__name__ ,static_folder='build',static_url_path='')
 cors = CORS(app)
+
+# Use a service account
+cred = credentials.Certificate('credentials.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('users')
 
 # config
 
@@ -13,6 +20,15 @@ cors = CORS(app)
 def home():
     return {'message': 'Hello, World! bert smells aaa'}
 
+@app.route('/list', methods=['GET'])
+def read():
+    # get all users
+    users_ref = db.collection('users')
+    docs = users_ref.stream()
+    print(docs)
+    return jsonify([doc.to_dict() for doc in docs])
+
+    
 
 @app.route('/')
 def serve():
