0de3b55fc6f714e9f80694d82caee271aba1d182
FrankM-7 <frankm-7@tamu.edu>
2023-01-28 16:11:19 -0600

    added register button in login

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
